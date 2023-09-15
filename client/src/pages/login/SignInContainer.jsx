eb7c631fb92185648649ca33f527bf93c0bd6ca3
VITTO SURYA TEDJA <vittosuryat.2021@scis.smu.edu.sg>
2023-09-15 17:09:13 +0800

    WIP: login page

---
+++ client/src/pages/login/SignInContainer.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { tokens } from '../../theme.js';
+import { Grid, Box, Button, useTheme, Typography, Link } from '@mui/material';
+import InputBase from '@mui/material/InputBase';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import LockIcon from '@mui/icons-material/Lock';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+const SignInContainer = (props) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <>
+            <Grid
+                xs={6}
+                id="signInContainer"
+                sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}
+            >
+                <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m: "0 0 5px 0" }}>Sign In With Password</Typography>
+                <div>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <AlternateEmailIcon />
+                        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Email" type='email' required />
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <LockIcon />
+                        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Password" type={showPassword ? 'text' : 'password'} />
+                        <Button
+                            sx={{ color: colors.primary[300] }}
+                            id="seePasswordBtn"
+                            onClick={() => setShowPassword(!showPassword)}
+                        >
+                            {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                        </Button>
+                    </Box>
+                </div>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                    <Typography variant="h6" mt={1}>Don't have an account?</Typography>
+                    <Typography variant="h6" mt={1}>
+                        Register with us {" "}
+                        <Link
+                            onClick={props.handleSignIn}
+                            sx={{ color: colors.primary[300], cursor: 'pointer' }}
+                        >
+                            here!
+                        </Link>
+                    </Typography>
+                </Box>
+                <Button sx={{ color: colors.primary[300], width: '100%' }}>Sign In</Button>
+            </Grid >
+        </>
+    );
+};
+
+export default SignInContainer;

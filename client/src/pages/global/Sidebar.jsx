9ed76dc14c7e867aed246dee7cd9c45ce6eb012f
Angie <louiseangiee@gmail.com>
2023-09-20 00:20:38 +0800

    ui-fix: cleaning up pages on sidebar

--- client/src/pages/global/Sidebar.jsx
+++ client/src/pages/global/Sidebar.jsx
@@ -175,13 +175,13 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-            <Item
+            {/* <Item
               title="Calendar"
               to="/calendar"
               icon={<CalendarTodayOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
-            />
+            /> */}
             <Item
               title="FAQ Page"
               to="/faq"
@@ -190,12 +190,12 @@ const Sidebar = () => {
               setSelected={setSelected}
             />
 
-            <Typography
+            {/* <Typography
               variant="h6"
               color={colors.grey[300]}
               sx={{ m: "15px 0 5px 20px" }}
-            >
-              Charts
+            > */}
+              {/* Charts
             </Typography>
             <Item
               title="Bar Chart"
@@ -224,7 +224,7 @@ const Sidebar = () => {
               icon={<MapOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
-            />
+            /> */}
           </Box>
         </Menu>
       </ProSidebar>

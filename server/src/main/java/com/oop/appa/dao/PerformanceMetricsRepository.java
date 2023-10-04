3bd58bf0885772c47ae71e05d97628b96a54d4b9
Davina Wijaya <phandavina@gmail.com>
2023-10-05 02:16:21 +0800

    change method name to suit entity

--- server/src/main/java/com/oop/appa/dao/PerformanceMetricsRepository.java
+++ server/src/main/java/com/oop/appa/dao/PerformanceMetricsRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.oop.appa.entity.PerformanceMetrics;
 
 public interface PerformanceMetricsRepository extends JpaRepository<PerformanceMetrics, Integer> {
-    List<PerformanceMetrics> findByPortfolioId(Integer portfolio_id);
+    List<PerformanceMetrics> findByPortfolioPortfolioId(Integer portfolioId);
 }

ab75c9bd97ae11ab1c3baeb70a56163f913b0fca
Jordian Renaldi <jrenaldi.2021@scis.smu.edu.sg>
2023-10-15 21:25:42 +0800

    Deletion of market data entity and dao

--- server/src/main/java/com/oop/appa/controller/MarketDataController.java
+++ server/src/main/java/com/oop/appa/controller/MarketDataController.java
@@ -1,6 +1,5 @@
 package com.oop.appa.controller;
 
-import com.oop.appa.entity.MarketData;
 import com.oop.appa.service.MarketDataService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,48 +19,6 @@ public class MarketDataController {
     @Autowired
     public MarketDataController(MarketDataService marketDataService) {
         this.marketDataService = marketDataService;
-    }
-
-    // GET endpoints
-    @GetMapping()
-    public List<MarketData> findAll() {
-        return marketDataService.findAll();
-    }
-
-    @GetMapping("/paged")
-    public Page<MarketData> findAllPaged(Pageable pageable) {
-        return marketDataService.findAllPaged(pageable);
-    }
-
-    @GetMapping("/stock/{stock_id}")
-    public Page<MarketData> findByUserId(@PathVariable String stock_id,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        return marketDataService.findByStockId(stock_id, pageable);
-    }
-
-    // POST endpoint for creating a new MarketData
-    @PostMapping
-    public void createMarketData(@RequestBody MarketData MarketData) {
-        marketDataService.save(MarketData);
-    }
-
-    // PUT endpoint for updating an existing MarketData
-    @PutMapping
-    public void updateMarketData(@RequestBody MarketData MarketData) {
-        marketDataService.save(MarketData);
-    }
-
-    // DELETE endpoints
-    @DeleteMapping("/{market_data_id}")
-    public void deleteById(@PathVariable Integer id) {
-        marketDataService.deleteById(id);
-    }
-
-    @DeleteMapping
-    public void delete(@RequestBody MarketData MarketData) {
-        marketDataService.delete(MarketData);
     }
 
     @GetMapping("/monthData")
--- server/src/main/java/com/oop/appa/service/MarketDataService.java
+++ server/src/main/java/com/oop/appa/service/MarketDataService.java
@@ -10,48 +10,16 @@ import org.springframework.web.reactive.function.client.WebClient;
 import io.github.cdimascio.dotenv.Dotenv;
 import com.fasterxml.jackson.databind.JsonNode;
 
-import com.oop.appa.dao.MarketDataRepository;
-import com.oop.appa.entity.MarketData;
-
 @Service
 public class MarketDataService {
 
-    private MarketDataRepository marketDataRepository;
     private final WebClient webClient;
     private static final Dotenv dotenv = Dotenv.load();
     private static final String ALPHA_VANTAGE_BASE_URL = "https://www.alphavantage.co";
 
     @Autowired
-    public MarketDataService(MarketDataRepository marketDataRepository, WebClient webClient) {
-        this.marketDataRepository = marketDataRepository;
+    public MarketDataService(WebClient webClient) {
         this.webClient = webClient;
-    }
-
-    // GET
-    public List<MarketData> findAll() {
-        return marketDataRepository.findAll();
-    }
-
-    public Page<MarketData> findAllPaged(Pageable pageable) {
-        return marketDataRepository.findAll(pageable);
-    }
-
-    public Page<MarketData> findByStockId(String stock_id, Pageable pageable) {
-        return marketDataRepository.findByStockStockSymbol(stock_id, pageable);
-    }
-
-    // POST and UPDATE
-    public void save(MarketData marketData) {
-        marketDataRepository.save(marketData);
-    }
-
-    // DELETE
-    public void delete(MarketData entity) {
-        marketDataRepository.delete(entity);
-    }
-
-    public void deleteById(Integer id) {
-        marketDataRepository.deleteById(id);
     }
 
     public JsonNode fetchMonthData(String symbol) {

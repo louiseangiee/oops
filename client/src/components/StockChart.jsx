e59cb5895f025b70a0a51180392f5f75aa819343
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2023-10-17 16:06:50 +0800

    stockchart linmk to api

--- client/src/components/StockChart.jsx
+++ client/src/components/StockChart.jsx
@@ -1,95 +1,151 @@
-import { TableCell, TableHead } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import { useTheme } from '@mui/material/styles';
+import { ButtonGroup, Button } from '@mui/material';
 import { tokens } from "../theme";
+import { colors } from '@mui/material';
+import { getAsync } from '../utils/utils';
+import { useCookies } from "react-cookie";
+
+const API_BASE_URL = 'http://localhost:8080';
 
 const CustomTooltip = ({ active, payload, label }) => {
-    if (active && payload && payload.length) {
-      return (
-        <div className="custom-tooltip" style={{ backgroundColor: 'white', padding: '10px', border: '1px solid #ccc', color:"black" }}>
-          <p className="label">{`Date: ${label}`}</p>
-          <p className="value">{`Value: $${payload[0].value.toFixed(2)}`}</p>
-        </div>
-      );
-    }
-  
-    return null;
-  };
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip" style={{ backgroundColor: 'white', padding: '10px', border: '1px solid #ccc', color: "black" }}>
+        <p className="label">{`Date: ${label}`}</p>
+        <p className="value">{`Value: $${payload[0].value.toFixed(2)}`}</p>
+      </div>
+    );
+  }
 
+  return null;
+};
 
 const StockChart = ({ chosenStock }) => {
-    const [chartData, setChartData] = useState([]);
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    
-    useEffect(() => {
-      // Fetch data based on the chosen stock
-      import(`../data/${chosenStock.code}Data.json`)
-        .then(module => {
-          loadData(module.default);
-        })
-        .catch(err => {
-          console.error("Failed to load stock data:", err);
-        });
-    }, [chosenStock]);
-  
-    const loadData = (data) => {
-      const parsedData = Object.entries(data["Time Series (Daily)"]).map(([date, values]) => {
-        return {
-          time: date,
-          value: parseFloat(values["4. close"])
-        };
-      });
-  
-      setChartData(parsedData);
+  const [chartData, setChartData] = useState([]);
+  const [timeSpan, setTimeSpan] = useState("1Y");
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [cookies] = useCookies(['accessToken']); // Get the accessToken from cookies
+
+  useEffect(() => {
+    fetchData();
+  }, [chosenStock, timeSpan]);
+
+  const fetchData = async () => {
+    let endpoint = '/stocks/OneYearData';  // Default to 1 year data
+
+    switch (timeSpan) {
+      case "1Y":
+        endpoint = '/stocks/OneYearData';
+        break;
+      case "1Q":
+        endpoint = '/stocks/OneQuarterData';
+        break;
+      case "1M":
+        endpoint = '/stocks/OneMonthData';
+        break;
+      case "1W":
+        endpoint = '/stocks/OneWeekData';
+        break;
+      default:
+        endpoint = '/stocks';
     }
 
-    
-  
-    const getTicks = (data) => {
-      const values = data.map(item => item.value);
-      const dataMin = Math.min(...values) - 10;
-      const dataMax = Math.max(...values) + 10;
-      const interval = (dataMax - dataMin) / 5; 
-  
-      const ticks = [];
-      for (let i = 0; i <= 5; i++) {
-        ticks.push(Math.round(dataMin + interval * i));
+    try {
+      const response = await getAsync(`${API_BASE_URL}${endpoint}?symbol=${chosenStock.code}`, cookies.accessToken); // Use the accessToken from cookies
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-  
-      return ticks;
-    };
-  
-    const ticks = getTicks(chartData);
-    const domainMin = Math.min(...ticks) - 10;
-    const domainMax = Math.max(...ticks) + 10;
+      const responseData = await response.json();
+      const data = responseData.map(item => ({
+        time: item.date,
+        value: parseFloat(item["4. close"])
+      }));
+      setChartData(data);
+    } catch (error) {
+      console.error("Failed to fetch stock data:", error);
+    }
+  };
 
-    const [selectedPoint, setSelectedPoint] = useState(null);
+  const getTicks = (data) => {
+    const values = data.map(item => item.value);
+    const dataMin = Math.min(...values) - 10;
+    const dataMax = Math.max(...values) + 10;
+    const interval = (dataMax - dataMin) / 5;
 
-    const handlePointClick = (data, index) => {
-        setSelectedPoint(data);
-    };
-  
-    return (
+    const ticks = [];
+    for (let i = 0; i <= 5; i++) {
+      ticks.push(Math.round(dataMin + interval * i));
+    }
+
+    return ticks;
+  };
+
+  const ticks = getTicks(chartData);
+  const domainMin = Math.min(...ticks) - 10;
+  const domainMax = Math.max(...ticks) + 10;
+
+  const [selectedPoint, setSelectedPoint] = useState(null);
+
+  const displayedData = chartData.reverse();  // Reverse the data
+
+  return (
     <div>
-        <ResponsiveContainer width="100%" height={300}>
-        <AreaChart  data={chartData}>
+      <ButtonGroup
+        variant="outlined"
+        style={{ borderColor: "white" }}
+        width="100%"
+      >
+        <Button
+          style={{
+            color: timeSpan === "1Y" ? "white" : "white",
+            backgroundColor: timeSpan === "1Y" ? colors.greenAccent[600] : "transparent"
+          }}
+          onClick={() => setTimeSpan("1Y")}
+          variant={timeSpan === "1Y" ? "contained" : "outlined"}>
+          1 Year
+        </Button>
+        <Button
+          style={{
+            color: timeSpan === "1Q" ? "white" : "white",
+            backgroundColor: timeSpan === "1Q" ? colors.greenAccent[600] : "transparent"
+          }}
+          onClick={() => setTimeSpan("1Q")}
+          variant={timeSpan === "1Q" ? "contained" : "outlined"}>
+          1 Quarter
+        </Button>
+        <Button
+          style={{
+            color: timeSpan === "1M" ? "white" : "white",
+            backgroundColor: timeSpan === "1M" ? colors.greenAccent[600] : "transparent"
+          }}
+          onClick={() => setTimeSpan("1M")}
+          variant={timeSpan === "1M" ? "contained" : "outlined"}>
+          1 Month
+        </Button>
+        <Button
+          style={{
+            color: timeSpan === "1W" ? "white" : "white",
+            backgroundColor: timeSpan === "1W" ? colors.greenAccent[600] : "transparent"
+          }}
+          onClick={() => setTimeSpan("1W")}
+          variant={timeSpan === "1W" ? "contained" : "outlined'}"} >
+          1 Week
+        </Button>
+      </ButtonGroup>
+      <ResponsiveContainer width="100%" height={300}>
+        <AreaChart data={displayedData}>
           <CartesianGrid stroke="#f5f5f5" />
           <XAxis dataKey="time" />
           <YAxis domain={[domainMin, domainMax]} ticks={ticks} />
           <Tooltip content={<CustomTooltip />} />
           <Area type="monotone" dataKey="value" stroke={colors.redAccent[400]} fill={colors.redAccent[500]} />
         </AreaChart>
-
-        
-
       </ResponsiveContainer>
-      
     </div>
-      
-      
-    );
+  );
 };
 
 export default StockChart;

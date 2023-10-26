b1b0e4aca5b3f77790d1c8cda6fb7b100e80c371
VITTO SURYA TEDJA <vittosuryat.2021@scis.smu.edu.sg>
2023-10-26 09:52:09 +0800

    change size

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -21,7 +21,7 @@ const Login = () => {
             <Grid container sx={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <img src={'../../Goldman_Sachs_Logos/logos_for_screen/GS_logo_PNG/Goldman_Sachs_Signature_Reverse.png'} alt="gs-logo" style={{ cursor: "pointer", width: "8%", height: "8%", position: "absolute", top: "40px", left: "40px" }} />
                 <Grid item md={8} id="welcomeGoldman" height={"100%"} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <Box height={"40%"} width={"40%"} sx={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', borderRadius: "20px" }}>
+                    <Box height={"50%"} width={"40%"} sx={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', borderRadius: "20px" }}>
                         <Typography variant="h1" sx={{ fontWeight: 'bold', color: 'black' }}>INVEST.</Typography>
                         <img
                             alt="invest"

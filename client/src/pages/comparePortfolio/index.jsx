5217b5d53ec0048c32bda5765f3e1805b3338090
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2023-10-27 12:21:02 +0800

    available portfolios dropdown

--- client/src/pages/comparePortfolio/index.jsx
+++ client/src/pages/comparePortfolio/index.jsx
@@ -20,12 +20,16 @@ const ComparePortfolio = () => {
         <Box m="20px">
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Header title="Compare Portfolio" subtitle="Compare Portfolio Performance" />
-
+                
         
             </Box>
+
+            
         </Box>
+
+        
     );
-    
+
 };
 
 export default ComparePortfolio;

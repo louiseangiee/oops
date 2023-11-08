3e764d495432bc708a8246fb752223a0990d6fe1
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2023-11-08 14:40:50 +0800

    ehe

--- client/src/components/StocksPerformanceTable.jsx
+++ client/src/components/StocksPerformanceTable.jsx
@@ -1,129 +1,138 @@
-import React, { useState, useMemo } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, Paper } from '@mui/material';
+import React, { useState, useEffect, useMemo } from 'react';
+import { useCookies } from 'react-cookie';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  Paper,
+  Box,
+  Typography,
+} from '@mui/material';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import { useEffect } from 'react';
 import { getAsync } from '../utils/utils';
-import { useCookies } from 'react-cookie';
+
+const TableHeaderCell = ({ id, label, onRequestSort, orderDirection, orderBy }) => (
+  <TableCell align="right">
+    <TableSortLabel
+      active={orderBy === id}
+      direction={orderBy === id ? orderDirection : 'asc'}
+      onClick={() => onRequestSort(id)}
+      IconComponent={orderDirection === 'asc' ? ArrowUpwardIcon : ArrowDownwardIcon}
+    >
+      {label}
+    </TableSortLabel>
+  </TableCell>
+);
 
 const ReturnsTable = ({ stockData, stockReturns }) => {
   const [orderDirection, setOrderDirection] = useState('asc');
+  const [orderBy, setOrderBy] = useState('percentage');
   const [cookies] = useCookies(['accessToken']);
-
   const [stockDetails, setStockDetails] = useState({});
-
-  const fetchStockDetails = async () => {
-    const details = {};
-    for (const stock of stockData) {
-      try {
-        // Assuming stock.symbol is the property that contains the stock's symbol
-        const weightResponse = await getAsync(`/portfolioStocks/${stock.portfolioId}/stocks/${stock.symbol}/weight`, cookies.accessToken);
-        const weightData = await weightResponse.json();
-
-        const weightedReturnResponse = await getAsync(`/portfolioStocks/${stock.portfolioId}/stocks/${stock.symbol}/calculateWeightedReturn`, cookies.accessToken);
-        const weightedReturnData = await weightedReturnResponse.json();
-
-        const annualisedReturnResponse = await getAsync(`/portfolioStocks/${stock.portfolioId}/stocks/${stock.symbol}/calculateAnnualisedReturn`, cookies.accessToken);
-        const annualisedReturnData = await annualisedReturnResponse.json();
-
-        details[stock.symbol] = {
-          weight: weightData,
-          weightedReturn: weightedReturnData,
-          annualisedReturn: annualisedReturnData
-        };
-      } catch (error) {
-        console.error('Failed to fetch stock details:', error);
-      }
-    }
-    setStockDetails(details);
-  };
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (stockData && stockData.length > 0) {
+    const fetchStockDetails = async () => {
+      setLoading(true);
+      setError(null);
+      const details = {};
+      try {
+        for (const stock of stockData.portfolioStocks) {
+          const responses = await Promise.all([
+            getAsync(`portfolioStocks/${stockData.portfolioId}/stocks/${stock.stockSymbol}/weight`, cookies.accessToken),
+            getAsync(`portfolioStocks/${stockData.portfolioId}/stocks/${stock.stockSymbol}/calculateWeightedReturn`, cookies.accessToken),
+            getAsync(`portfolioStocks/${stockData.portfolioId}/stocks/${stock.stockSymbol}/calculateAnnualisedReturn`, cookies.accessToken),
+          ]);
+          const [weightData, weightedReturnData, annualisedReturnData] = await Promise.all(responses.map(res => res.json()));
+          details[stock.stockSymbol] = {
+            weight: weightData,
+            weightedReturn: weightedReturnData,
+            annualisedReturn: annualisedReturnData,
+          };
+        }
+        setStockDetails(details);
+      } catch (error) {
+        console.error('Failed to fetch stock details:', error);
+        setError('Failed to fetch stock details');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (stockData && stockData.portfolioStocks && stockData.portfolioStocks.length > 0) {
       fetchStockDetails();
     }
-  }, [stockData]);
-
+  }, [stockData, cookies.accessToken]);
 
   const sortedStockReturns = useMemo(() => {
-    if (!stockReturns || typeof stockReturns !== 'object' || Object.keys(stockDetails).length === 0) {
-      return [];
-    }
-  
-    const entries = Object.entries(stockReturns).map(([name, data]) => ({
-      name,
-      ...data,
-      // Spread in additional details by matching with the stock symbol
-      ...stockDetails[name]
-    }));
-  
-    // Sorting logic
-    return entries.sort((a, b) => {
-      const valueA = a.percentage !== undefined ? a.percentage : Number.NEGATIVE_INFINITY; // Treat undefined as the lowest value
-      const valueB = b.percentage !== undefined ? b.percentage : Number.NEGATIVE_INFINITY;
-  
-      if (orderDirection === 'asc') {
-        return valueA - valueB;
-      } else {
-        return valueB - valueA;
-      }
-    });
-  }, [stockReturns, orderDirection, stockDetails]);
-  
+    return Object.entries(stockReturns || {})
+      .map(([symbol, data]) => ({
+        symbol,
+        ...data,
+        ...stockDetails[symbol],
+      }))
+      .sort((a, b) => {
+        const valueA = a[orderBy] ?? Number.NEGATIVE_INFINITY;
+        const valueB = b[orderBy] ?? Number.NEGATIVE_INFINITY;
+        return (orderDirection === 'asc' ? valueA - valueB : valueB - valueA);
+      });
+  }, [stockReturns, stockDetails, orderBy, orderDirection]);
 
-  const handleSortRequest = () => {
-    setOrderDirection(orderDirection === 'asc' ? 'desc' : 'asc');
+  const handleRequestSort = (property) => {
+    const isAsc = orderBy === property && orderDirection === 'asc';
+    setOrderBy(property);
+    setOrderDirection(isAsc ? 'desc' : 'asc');
   };
-  // Set the height for the visible area of the table
-  const maxTableBodyHeight = 'calc(5 * 53px)'; 
+
+  if (loading) return <Box sx={{ display: 'flex', justifyContent: 'center' }}><Typography>Loading...</Typography></Box>;
+  if (error) return <Box sx={{ display: 'flex', justifyContent: 'center' }}><Typography color="error">{error}</Typography></Box>;
 
   return (
-    <TableContainer component={Paper} style={{ maxHeight: 'calc(5 * 53px + 52px)' }}> {/* additional height for the header */}
+    <TableContainer component={Paper}>
       <Table aria-label="simple table" stickyHeader>
         <TableHead>
           <TableRow>
             <TableCell>Stock</TableCell>
-            <TableCell align="right">Weight (%)</TableCell>
-            <TableCell align="right"><TableSortLabel
-              active={true} // You might want to make this dynamic if you have multiple sortable columns
-              direction={orderDirection}
-              onClick={() => handleSortRequest('percentage')} // Modify to handle different columns
-              IconComponent={orderDirection === 'asc' ? ArrowDownwardIcon : ArrowUpwardIcon} // Icons change with direction
-            >
-              Return (%)
-            </TableSortLabel></TableCell>
+            <TableHeaderCell
+              id="weight"
+              label="Weight (%)"
+              onRequestSort={handleRequestSort}
+              orderDirection={orderDirection}
+              orderBy={orderBy}
+            />
+            <TableHeaderCell
+              id="percentage"
+              label="Return (%)"
+              onRequestSort={handleRequestSort}
+              orderDirection={orderDirection}
+              orderBy={orderBy}
+            />
             <TableCell align="right">Returns ($)</TableCell>
             <TableCell align="right">Weighted Return (%)</TableCell>
             <TableCell align="right">Annualised Return (%)</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody style={{ overflow: 'auto', maxHeight: maxTableBodyHeight }}>
+        <TableBody>
           {sortedStockReturns.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">
-                {row.weight ? `${(row.weight * 100).toFixed(2)}%` : 'N/A'}
-              </TableCell>
-              <TableCell align="right" style={{ color: row.percentage < 0 ? 'red' : 'green' }}>
-                {typeof row.percentage === 'number' ? row.percentage.toFixed(2) : 'N/A'}%
-              </TableCell>
-              <TableCell align="right">
-                {typeof row.actualValue === 'number' ? row.actualValue.toFixed(2) : 'N/A'}
-              </TableCell>
-              <TableCell align="right">
-                {row.weightedReturn ? `${row.weightedReturn.toFixed(2)}%` : 'N/A'}
-              </TableCell>
-              <TableCell align="right">
-                {row.annualisedReturn ? `${row.annualisedReturn.toFixed(2)}%` : 'N/A'}
-              </TableCell>
+            <TableRow key={row.symbol}>
+              <TableCell component="th" scope="row">{row.symbol}</TableCell>
+              <TableCell align="right">{row.weight}</TableCell>
+              <TableCell align="right">{row.percentage}</TableCell>
+              <TableCell align="right">{row.absolute}</TableCell>
+              <TableCell align="right">{row.weightedReturn}</TableCell>
+              <TableCell align="right">{row.annualisedReturn}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
-}
+};
 
 export default ReturnsTable;
